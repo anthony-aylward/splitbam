0c516ed8ef410cc4c38004b92b7422fe01721cd6
Anthony Aylward <aaylward@eng.ucsd.edu>
2020-12-24 17:04:56 -0800

    advance

--- splitbam/splitbam.py
+++ splitbam/splitbam.py
@@ -31,9 +31,26 @@ def parse_arguments():
         metavar='<path/to/tmp/dir>',
         help='directory for temporary files'
     )
+    return parser.parse_args()
+
 
 def main():
-    with tempfile.TemporaryDirectory(dir) as temp_dir:
+    args = parse_arguments()
+    with tempfile.TemporaryFile() as temp_file:
+        temp_file_name = temp_file.name
+    pysam.sort('-n', args.input, temp_file_name)
+    for out in args.output:
+        pysam.view('-H', args.input, out)
+    with open(args.input, 'r') as f:
+        with subprocess.Popen(
+            ('samtools', 'view'), stdin=f, stdout=subprocess.PIPE
+        ) as view:
+            subprocess.run(
+                ('awk', ),
+                stdin=view.stdout
+            )
+
+
 
     
     
