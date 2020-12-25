f739c830bc8c3090ecef973017321141874c0b79
Anthony Aylward <aaylward@eng.ucsd.edu>
2020-12-25 10:06:32 -0800

    test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='splitbam',
-    version='0.0.8',
+    version='0.0.9',
     author='Anthony Aylward',
     author_email='aaylward@eng.ucsd.edu',
     description='Split a BAM file into two subsamples',
--- splitbam/splitbam.py
+++ splitbam/splitbam.py
@@ -55,6 +55,24 @@ def parse_arguments():
     return parser.parse_args()
 
 
+def extract_records(input_bam, output_sam, condition='NR%4<2')
+    pysam.view(
+        '-@', str(args.processes - 1),
+        '-H',
+        '-o', output_sam,
+        input_bam
+    )
+    with open(input_bam, 'r') as f0, open(output_sam, 'a') as f1:
+        with subprocess.Popen(
+            ('samtools', 'view'), stdin=f, stdout=subprocess.PIPE
+        ) as view:
+            subprocess.Popen(
+                ('awk', f'{{if({condition}){{print}}}}'),
+                stdin=view.stdout,
+                stdout=f1
+            )
+
+
 def main():
     args = parse_arguments()
     with tempfile.TemporaryDirectory(dir=args.tmp_dir) as temp_dir:
@@ -69,20 +87,13 @@ def main():
             '-o', temp_in,
             args.input
         )
-        for out in temp_out0, temp_out1:
-            pysam.view(
-                '-@', str(args.processes - 1),
-                '-H',
-                '-o', out,
-                args.input
-            )
-        with open(temp_in, 'r') as f:
-            with subprocess.Popen(
-                ('samtools', 'view'), stdin=f, stdout=subprocess.PIPE
-            ) as view:
-                subprocess.run(
-                    ('awk', f'{{if(NR%4<2){{print >> "{temp_out0}}}"}} else {{print >> "{temp_out1}}}"}}}}'),
-                    stdin=view.stdout,
+        with Pool(processes=max(args.processes, 2)) as pool:
+            pool.starmap(
+                extract_records,
+                (
+                    (temp_in, temp_out0, 'NR%4<2'),
+                    (temp_in, temp_out1, 'NR%4>=2')
                 )
+            )
         for tmp_out, out in (temp_out0, args.out0), (temp_out1, args.out1):
             pysam.view('-@', str(args.processes - 1), '-bh', '-o', out, tmp_out)
